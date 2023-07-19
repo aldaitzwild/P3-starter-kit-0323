080988292f2b502ca66f196ad645a61f502bf3fd
Spolito24 <baptisterenier69@gmail.com>
2023-07-19 13:56:16 +0200

    work on home page front

---
+++ src/Controller/TodoListController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\TodoList;
+use App\Form\TodoListType;
+use App\Repository\TodoListRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/todoList', name: 'todo_list_')]
+class TodoListController extends AbstractController
+{
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(TodoListRepository $todoListRepository): Response
+    {
+        return $this->render('todo_list/index.html.twig', [
+            'todo_lists' => $todoListRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $todoList = new TodoList();
+        $form = $this->createForm(TodoListType::class, $todoList);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($todoList);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('todo_list_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('todo_list/new.html.twig', [
+            'todo_list' => $todoList,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(TodoList $todoList): Response
+    {
+        return $this->render('todo_list/show.html.twig', [
+            'todo_list' => $todoList,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, TodoList $todoList, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(TodoListType::class, $todoList);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('todo_list_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('todo_list/edit.html.twig', [
+            'todo_list' => $todoList,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
+    public function delete(Request $request, TodoList $todoList, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$todoList->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($todoList);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('todo_list_sindex', [], Response::HTTP_SEE_OTHER);
+    }
+}
